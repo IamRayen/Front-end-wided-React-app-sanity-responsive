15182e3e85160ce086916652f728ab38a974ce7b
Rayen Labidi <89748200+IamRayen@users.noreply.github.com>
2022-02-02 19:42:02 +0100

    fixing...

--- src/Views/Articles.js
+++ src/Views/Articles.js
@@ -2,43 +2,45 @@ import React, { useState, useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
 import client from "../client";
 import { Link } from "react-router-dom";
-import imageUrlBuilder from '@sanity/image-url'
-import myConfiguredSanityClient from '../client'
+import imageUrlBuilder from "@sanity/image-url";
+import myConfiguredSanityClient from "../client";
 import SkeletonArticles from "../Skeletons/SkeletonArticles";
-import Aos from "aos"
-import "aos/dist/aos.css"
-
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Articles = () => {
     const [articles, setArticles] = useState(null);
-    
-    const builder = imageUrlBuilder(myConfiguredSanityClient)
-    
-    const urlFor = (source) =>{
-        return builder.image(source)
-    }
 
-    useEffect(async () => {
-        try {
-            const data = 
-            await client.fetch(`*[_type == "post"] | order(publishedAt desc,title) {
-                title,
-                introduction,
-                slug,
-                mainImage{
-                    asset->{
-                        _id,
-                        url
-                    }
-                },
-                publishedAt
-            }`);
-            setArticles(data);
-            Aos.init({})
-        } catch (error) {
-            console.log(error);
-        }
-    },[articles])
+    const builder = imageUrlBuilder(myConfiguredSanityClient);
+
+    const urlFor = (source) => {
+        return builder.image(source);
+    };
+
+    useEffect(() => {
+        const Function = async () => {
+            try {
+                const data =
+                    await client.fetch(`*[_type == "post"] | order(publishedAt desc,title) {
+                    title,
+                    introduction,
+                    slug,
+                    mainImage{
+                        asset->{
+                            _id,
+                            url
+                        }
+                    },
+                    publishedAt
+                }`);
+                setArticles(data);
+                Aos.init({});
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        Function();
+    }, []);
 
     const fixDate = (dateToFix) => {
         const datetime = new Date(dateToFix);
@@ -58,20 +60,34 @@ const Articles = () => {
                         key={post.slug.current}
                         style={{ textDecoration: "none" }}
                     >
-                        <Card className="Cards" data-aos="fade-up" style={{ width: "350px",marginBottom:"40px" }}>
-                            
+                        <Card
+                            className="Cards"
+                            data-aos="fade-up"
+                            style={{ width: "350px", marginBottom: "40px" }}
+                        >
                             <Card.Img
                                 variant="top"
-                                src={urlFor(post.mainImage).height(400).width(350).quality(80).url()}
+                                src={urlFor(post.mainImage)
+                                    .height(400)
+                                    .width(350)
+                                    .quality(80)
+                                    .url()}
                                 className="img-fluid"
                             />
 
                             <Card.Body>
-                                <Card.Title style={{color:"black"}}>{post.title}</Card.Title>
-                                <Card.Text style={{color:"grey"}}>
+                                <Card.Title style={{ color: "black" }}>
+                                    {post.title}
+                                </Card.Title>
+                                <Card.Text style={{ color: "grey" }}>
                                     {post.introduction}
                                 </Card.Text>
-                                <Button variant="dark" style={{borderRadius:"0"}}>Go To Article</Button>
+                                <Button
+                                    variant="dark"
+                                    style={{ borderRadius: "0" }}
+                                >
+                                    Go To Article
+                                </Button>
                             </Card.Body>
 
                             <Card.Footer>
@@ -82,7 +98,7 @@ const Articles = () => {
                         </Card>
                     </Link>
                 ))}
-                {!articles && [1,2,3,4,5,6].map(el => <SkeletonArticles/>)}
+            {!articles && [1, 2, 3, 4, 5, 6].map((el) => <SkeletonArticles />)}
         </div>
     );
 };
