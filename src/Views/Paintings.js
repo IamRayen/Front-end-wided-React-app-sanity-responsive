0d6a638a5703a87cb9e49a00552acce103502de7
Rayen Labidi <89748200+IamRayen@users.noreply.github.com>
2022-02-08 10:09:11 +0100

    basic filter complete

--- src/Views/Paintings.js
+++ src/Views/Paintings.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import imageUrlBuilder from "@sanity/image-url";
 import myConfiguredSanityClient from "../client";
 import { Card, Button } from "react-bootstrap";
-import SkeletonArticles from '../Skeletons/SkeletonArticles';
+import SkeletonArticles from "../Skeletons/SkeletonArticles";
 import client from "../client";
 import { Link } from "react-router-dom";
 import Aos from "aos";
@@ -34,17 +34,16 @@ const Paintings = () => {
                     },
                     publishedAt
                 }`);
-                const data2 = 
-                    await client.fetch(`*[_type == "category"]
-                    `)
+                const data2 = await client.fetch(`*[_type == "category"]
+                    `);
                 setPaintings(data);
-                setCategories(data2)
+                setCategories(data2);
                 Aos.init({});
             } catch (error) {
                 console.log(error);
             }
         };
-        Function()
+        Function();
     }, []);
 
     const fixDate = (dateToFix) => {
@@ -55,68 +54,102 @@ const Paintings = () => {
         });
         return truedate;
     };
-    const [filter, setFilter] = useState(null);
-console.log(categories);
-console.log(filter);
+    const Filter = (el) => {
+        if (filter === "all") {
+            return true;
+        } else {
+            return el.categories[0]._ref === filter;
+        }
+    };
+    const [filter, setFilter] = useState("all");
+    const Checkbox = (el) => {
+        el ? setFilter(el._id) : setFilter("all");
+    };
 
-  return(
-      <div>
-          <div className='container-lg border my-4 categorie d-flex align-items-center'>
-              <h1 className='display-6 me-5'>Categories</h1>
-              <div className='d-flex justify-content-center col'>
-              {categories && categories.map(el =><label className='px-4'><input onClick={()=>filter !== null ? setFilter(null) : setFilter(el._id)} type="checkbox"/><span>{el.title}</span></label>)}
-              </div>
-          </div>
-    <div className="container-xl d-flex justify-content-evenly flex-wrap">
-    {paintings &&
-        paintings.map((painting) => (
-            <Link
-                to={"/painting/" + painting.slug.current}
-                key={painting.slug.current}
-                style={{ textDecoration: "none" }}
-            >
-                <Card
-                    className="Cards"
-                    data-aos="fade-up"
-                    style={{ width: "350px", marginBottom: "40px",border:"white" }}
-                >
-                    <Card.Img
-                        variant="top"
-                        src={urlFor(painting.mainImage)
-                            .height(400)
-                            .width(350)
-                            .quality(80)
-                            .url()}
-                        className="img-fluid"
-                    />
+    return (
+        <div>
+            <div className="container-lg border my-4 categorie d-flex align-items-center">
+                <h1 className="display-6 me-5">Categories</h1>
+                <div className="d-flex justify-content-center col">
+                    <label className="px-4">
+                        <input
+                            onClick={() => Checkbox()}
+                            defaultChecked
+                            type="radio"
+                            name="123"
+                        />
+                        <span>all</span>
+                    </label>
+                    {categories &&
+                        categories.map((el) => (
+                            <label className="px-4">
+                                <input
+                                    onClick={() => Checkbox(el)}
+                                    type="radio"
+                                    name="123"
+                                />
+                                <span>{el.title}</span>
+                            </label>
+                        ))}
+                </div>
+            </div>
+            <div className="container-xl d-flex justify-content-evenly flex-wrap">
+                {paintings &&
+                    paintings
+                        .filter((el) => Filter(el))
+                        .map((painting) => (
+                            <Link
+                                to={"/painting/" + painting.slug.current}
+                                key={painting.slug.current}
+                                style={{ textDecoration: "none" }}
+                            >
+                                <Card
+                                    className="Cards"
+                                    data-aos="fade-up"
+                                    style={{
+                                        width: "350px",
+                                        marginBottom: "40px",
+                                        border: "white",
+                                    }}
+                                >
+                                    <Card.Img
+                                        variant="top"
+                                        src={urlFor(painting.mainImage)
+                                            .height(400)
+                                            .width(350)
+                                            .quality(80)
+                                            .url()}
+                                        className="img-fluid"
+                                    />
 
-                    <Card.Body>
-                        <Card.Title style={{ color: "black" }}>
-                            {painting.title}
-                        </Card.Title>
-                        <Card.Text style={{ color: "grey" }}>
-                            {painting.introduction}
-                        </Card.Text>
-                        <Button
-                            variant="dark"
-                            style={{ borderRadius: "0" }}
-                        >
-                            Go To Painting
-                        </Button>
-                    </Card.Body>
+                                    <Card.Body>
+                                        <Card.Title style={{ color: "black" }}>
+                                            {painting.title}
+                                        </Card.Title>
+                                        <Card.Text style={{ color: "grey" }}>
+                                            {painting.introduction}
+                                        </Card.Text>
+                                        <Button
+                                            variant="dark"
+                                            style={{ borderRadius: "0" }}
+                                        >
+                                            Go To Painting
+                                        </Button>
+                                    </Card.Body>
 
-                    <Card.Footer>
-                        <small className="text-muted">
-                            {fixDate(painting.publishedAt)}
-                        </small>
-                    </Card.Footer>
-                </Card>
-            </Link>
-        ))}
-    {!paintings && [1, 2, 3, 4, 5, 6].map((el) => <SkeletonArticles />)}
-</div>
-</div>
-  )
+                                    <Card.Footer>
+                                        <small className="text-muted">
+                                            {fixDate(painting.publishedAt)}
+                                        </small>
+                                    </Card.Footer>
+                                </Card>
+                            </Link>
+                        ))}
+                {!paintings &&
+                    [1, 2, 3, 4, 5, 6].map((el) => <SkeletonArticles />)}
+            </div>
+        </div>
+    );
 };
 
 export default Paintings;
